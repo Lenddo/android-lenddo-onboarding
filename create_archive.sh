8aadd9bb32ef75e0414d2ca38471f4eba71f555f
Joseph Emmanuel Dayo <joseph.dayo@gmail.com>
2015-09-30 13:57:27 +0800

    remove packaging scripts

--- create_archive.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-hg archive ../LenddoSDK-android.zip
-zip -d ../LenddoSDK-android.zip "LenddoSDK-android/prepare_distribution.sh"
-zip -d ../LenddoSDK-android.zip "LenddoSDK-android/create_archive.sh"
--- prepare_distribution.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cp LenddoSDK/build/intermediates/bundles/release/classes.jar LenddoSDK/libs/lenddosdk.jar
-
-zip -d LenddoSDK/libs/lenddosdk.jar "com/lenddo/sdk/BuildConfig.class"
